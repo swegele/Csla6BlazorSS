ff06ffb763826951cc32b15695755f47966040c4
swegele <swegele@users.noreply.github.com>
2022-01-25 18:08:39 -0700

    updating demo to show latest behavior

--- BlazorApp1/Services/BlazorApplicationContextManager.cs
+++ BlazorApp1/Services/BlazorApplicationContextManager.cs
@@ -1,46 +1,100 @@
 ﻿using Csla;
 using Csla.Core;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+using System.Security.Principal;
 
 namespace BlazorApp1.Services;
 
-public class BlazorApplicationContextManager : IContextManager
+/// <summary>
+/// Application context manager that uses HttpContextAccessor when 
+/// resolving HttpContext to store context values.
+/// </summary>
+public class BlazorApplicationContextManager : IContextManager, IDisposable
 {
-    private System.Security.Principal.IPrincipal? Principal { get; set; }
-    private ContextDictionary LocalContext { get; set; } = new ContextDictionary();
-    private ContextDictionary ClientContext { get; set; } = new ContextDictionary();
-    private ApplicationContext? _applicationContext { get; set; }
-    private Guid _guid = Guid.NewGuid(); //testing to see unique instances in DI
+    private ContextDictionary LocalContext { get; set; }
+    private ContextDictionary ClientContext { get; set; }
+    private IPrincipal CurrentPrincipal { get; set; }
+    private readonly ClaimsPrincipal UnauthenticatedPrincipal = new();
+    private bool disposedValue;
+
+    /// <summary>
+    /// Gets the current HttpContext instance.
+    /// </summary>
+    protected AuthenticationStateProvider AuthenticationStateProvider { get; private set; }
+
+    /// <summary>
+    /// Gets or sets a reference to the current ApplicationContext.
+    /// </summary>
+    public ApplicationContext ApplicationContext { get; set; }
 
     /// <summary>
     /// Creates an instance of the object, initializing it
     /// with the required IServiceProvider.
     /// </summary>
-    /// <param name="httpContextAccessor">HttpContext accessor</param>
-    public BlazorApplicationContextManager()
+    /// <param name="authenticationStateProvider">AuthenticationStateProvider service</param>
+    public BlazorApplicationContextManager(AuthenticationStateProvider authenticationStateProvider)
     {
+        AuthenticationStateProvider = authenticationStateProvider;
+        CurrentPrincipal = UnauthenticatedPrincipal;
+        AuthenticationStateProvider.AuthenticationStateChanged += AuthenticationStateProvider_AuthenticationStateChanged;
+        InitializeUser();
+    }
+
+    private void InitializeUser()
+    {
+        if (AuthenticationStateProvider is IHostEnvironmentAuthenticationStateProvider hostEnvironmentAuthProvider)
+        {
+            //i've no idea what i'm doing except to call set before get like error says
+            var task = new Task<AuthenticationState>(() => new AuthenticationState(UnauthenticatedPrincipal));
+
+            //the following set triggers the authentication state changed so why do it again below?
+            hostEnvironmentAuthProvider.SetAuthenticationState(task);
+        }
+        else
+        {
+            //??
+            AuthenticationStateProvider_AuthenticationStateChanged(AuthenticationStateProvider.GetAuthenticationStateAsync());
+        }
+    }
+
+    private void AuthenticationStateProvider_AuthenticationStateChanged(Task<AuthenticationState> task)
+    {
+        task.ContinueWith((t) =>
+        {
+            if (task.IsCompletedSuccessfully && task.Result != null)
+                CurrentPrincipal = task.Result.User;
+            else
+                CurrentPrincipal = UnauthenticatedPrincipal;
+        });
+    }
+
+    /// <summary>
+    /// Gets a value indicating whether this
+    /// context manager is valid for use in
+    /// the current environment.
+    /// </summary>
+    public bool IsValid
+    {
+        get { return true; }
     }
 
     /// <summary>
     /// Gets the current principal.
     /// </summary>
-    public System.Security.Principal.IPrincipal GetUser()
+    public IPrincipal GetUser()
     {
-        var result = Principal;
-        if (result == null)
-        {
-            result = new Csla.Security.CslaClaimsPrincipal();
-            SetUser(result);
-        }
-        return result;
+        return CurrentPrincipal;
     }
 
     /// <summary>
-    /// Sets the current principal.
+    /// Not supported. Use the aspnetcore framework to set 
+    /// the current principal.
     /// </summary>
     /// <param name="principal">Principal object.</param>
-    public void SetUser(System.Security.Principal.IPrincipal principal)
-    {
-        Principal = principal;
+    public virtual void SetUser(IPrincipal principal)
+    {        
+        throw new NotSupportedException(nameof(SetUser));
     }
 
     /// <summary>
@@ -48,6 +102,8 @@ public class BlazorApplicationContextManager : IContextManager
     /// </summary>
     public ContextDictionary GetLocalContext()
     {
+        if (LocalContext == null)
+            LocalContext = new ContextDictionary();
         return LocalContext;
     }
 
@@ -66,6 +122,8 @@ public class BlazorApplicationContextManager : IContextManager
     /// <param name="executionLocation"></param>
     public ContextDictionary GetClientContext(ApplicationContext.ExecutionLocations executionLocation)
     {
+        if (ClientContext == null)
+            ClientContext = new ContextDictionary();
         return ClientContext;
     }
 
@@ -80,19 +138,28 @@ public class BlazorApplicationContextManager : IContextManager
     }
 
     /// <summary>
-    /// Gets or sets a reference to the current ApplicationContext.
+    /// Dispose this object's resources.
     /// </summary>
-    public virtual ApplicationContext ApplicationContext
+    /// <param name="disposing"></param>
+    protected virtual void Dispose(bool disposing)
     {
-        get
+        if (!disposedValue)
         {
-            return _applicationContext;
-        }
-        set
-        {
-            _applicationContext = value;
+            if (disposing)
+            {
+                AuthenticationStateProvider.AuthenticationStateChanged -= AuthenticationStateProvider_AuthenticationStateChanged;
+            }
+            disposedValue = true;
         }
     }
 
-    public bool IsValid => true;
+    /// <summary>
+    /// Dispose this object's resources.
+    /// </summary>
+    public void Dispose()
+    {
+        // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
+    }
 }
--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Demo to show possible issues for csla blazor server side
 
 
 DEMO #1
-- Put a breakpoint in catch block of DemoRefreshService and start the app
-- Notice error from Csla.Blazor.ApplicationContextManager.
+- Put a breakpoint in catch block of DemoRefreshService and start the app with no changes needed
+- Notice the hosted service (which starts before a page loads) causes error in the Csla.Blazor.ApplicationContextManager.
 - Now in Program.cs, uncomment the DEMO #1 custom application context manager 
-- re-run no error
+- re-run and notice now the error moves down to the SetUser method.
